a41eb0fc523b8cf5b7b88dca4189b4e29d5f7ed9
sahana-h <sahanahariharan@gmail.com>
2024-02-24 14:31:08 -0600

    try

--- code/obstacle_avoiding_car.py
+++ code/obstacle_avoiding_car.py
@@ -3,46 +3,86 @@ import time
 from src import motor as motor_module
 import numpy as np
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+if __name__ == '__main__':
 
-# Define motor pins
-motor_pins = {
-    "speed": 13,
-    "control1": 5,
-    "control2": 6
-}
+    motor1 = motor_module.Motor({
+        "pins": {
+            "speed": 13,
+            "control1": 5,
+            "control2": 6
+        }
+    })
 
-# Initialize motors
-motor1 = motor_module.Motor({"pins": motor_pins})
-motor2 = motor_module.Motor({"pins": motor_pins})  # Assuming same motor configuration
+    motor2 = motor_module.Motor({
+        "pins": {
+            "speed": 12,
+            "control1": 7,
+            "control2": 8
+        }
+    })
 
-# Define motor speeds
-speeds = list(np.linspace(0, 1, 11)) + list(np.linspace(0.9, 0, 10))
+    speeds = list(np.linspace(0, 1, 11)) + list(np.linspace(0.9, 0, 10))
 
-# Define delay time
-dt = 0.25
+    dt = 0.25
+    motor1.stop()
+    motor2.stop()
+    time.sleep(dt)
 
-# Define ultrasonic sensor pins
-TRIG = 17
-ECHO = 27
+    for speed in speeds:
+        print('Motor forward at {}% speed'.format(speed * 100))
+        motor1.forward(speed)
+        motor2.forward(speed)
+        time.sleep(dt)
 
-# Set up ultrasonic sensor
-GPIO.setup(TRIG, GPIO.OUT)
-GPIO.setup(ECHO, GPIO.IN)
+    for speed in speeds:
+        print('Motor backward at {}% speed'.format(speed * 100))
+        motor1.backward(speed)
+        motor2.backward(speed)
+        time.sleep(dt)
 
-def stop_motors():
     motor1.stop()
     motor2.stop()
 
-def move_forward(speed):
-    motor1.forward(speed)
-    motor2.forward(speed)
+# # Define motor pins
+# motor_pins = {
+#     "speed": 13,
+#     "control1": 5,
+#     "control2": 6
+# }
 
-def move_backward(speed):
-    motor1.backward(speed)
-    motor2.backward(speed)
+# # Initialize motors
+# motor1 = motor_module.Motor({"pins": motor_pins})
+# motor2 = motor_module.Motor({"pins": motor_pins})  # Assuming same motor configuration
 
-def turn_right():
-    motor1.forward(0.5)  # Adjust speed for turning
-    motor2.backwar
+# motor1.forward(12)
+# motor2.forward(12)
+
+# # Define motor speeds
+# speeds = list(np.linspace(0, 1, 11)) + list(np.linspace(0.9, 0, 10))
+
+# # Define delay time
+# dt = 0.25
+
+# # Define ultrasonic sensor pins
+# TRIG = 17
+# ECHO = 27
+
+# # Set up ultrasonic sensor
+# GPIO.setup(TRIG, GPIO.OUT)
+# GPIO.setup(ECHO, GPIO.IN)
+
+# def stop_motors():
+#     motor1.stop()
+#     motor2.stop()
+
+# def move_forward(speed):
+#     motor1.forward(speed)
+#     motor2.forward(speed)
+
+# def move_backward(speed):
+#     motor1.backward(speed)
+#     motor2.backward(speed)
+
+# def turn_right():
+#     motor1.forward(0.5)  # Adjust speed for turning
+#     motor2.backward()
