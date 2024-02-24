16a9ec4233eb436a989a67c54274a76e51cd5c7e
sahana-h <sahanahariharan@gmail.com>
2024-02-24 17:38:32 -0600

    try

--- code/cameraTry.py
+++ code/cameraTry.py
@@ -1,58 +1,27 @@
-# from src import camera as camera_module
-# import cv2
-
-# if __name__ == '__main__':
-
-#     camera = camera_module.Camera({
-#         "show_preview": True
-#     })
-
-#     # Continuously capture frames and display them
-#     while True:
-#         camera.capture()
-#         image_array = camera.image_array
-
-#         # Display the image using OpenCV
-#         cv2.imshow("Camera Video", image_array)
-
-#         # Check for 'q' key to exit the loop
-#         if cv2.waitKey(1) & 0xFF == ord('q'):
-#             break
-
-#     # Release the camera and close OpenCV window
-#     camera.release()
-#     cv2.destroyAllWindows()
-
-import cv2
-from picamera import PiCamera
-from picamera.array import PiRGBArray
+from src import camera as camera_module
 import time
+import cv2
 
-# Initialize the camera
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 30
-rawCapture = PiRGBArray(camera, size=(640, 480))
+if __name__ == '__main__':
 
-# Allow the camera to warmup
-time.sleep(0.1)
+    total_seconds = 60
+    sample_hz = 10
 
-# Capture video frames
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # Grab the raw NumPy array representing the image
-    image = frame.array
+    camera = camera_module.Camera({
+        "show_preview": False
+    })
+    start_time = time.time()
+
+    while time.time() - start_time < total_seconds:
+        camera.capture()
+        print(camera.image_array)
+        
+        cv_image = cv2.cvtColor(camera.image_array, cv2.COLOR_BGR2RGB)
 
     # Display the image
-    cv2.imshow("Video", image)
-    
-    # Clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
+        cv2.imshow("Image", cv_image)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    # Check for the 'q' key to exit the loop
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        break
-
-# Clean up
-cv2.destroyAllWindows()
-camera.close()
+        time.sleep(max(0, 1/sample_hz -
+                       (time.time() - start_time)))
