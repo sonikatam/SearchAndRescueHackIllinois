e5c63fb113a46aa48861b042d3720e671c435d28
sahana-h <sahanahariharan@gmail.com>
2024-02-24 18:10:38 -0600

    opencv

--- code/cameraTry.py
+++ code/cameraTry.py
@@ -1,27 +1,19 @@
-from src import camera as camera_module
-import time
 import cv2
 
-if __name__ == '__main__':
+cap = cv2.VideoCapture(0)
 
-    total_seconds = 60
-    sample_hz = 10
+while True:
+    ret, frame = cap.read()
+    
+    # Check if the frame is valid
+    if not ret:
+        break
 
-    camera = camera_module.Camera({
-        "show_preview": False
-    })
-    start_time = time.time()
+    cv2.imshow('Video Capture', frame)
 
-    while time.time() - start_time < total_seconds:
-        camera.capture()
-        print(camera.image_array)
-        
-        cv_image = cv2.cvtColor(camera.image_array, cv2.COLOR_BGR2RGB)
+    # Press 'q' to exit the loop and close the window
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-    # Display the image
-        cv2.imshow("Image", cv_image)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-
-        time.sleep(max(0, 1/sample_hz -
-                       (time.time() - start_time)))
+cap.release()
+cv2.destroyAllWindows()
